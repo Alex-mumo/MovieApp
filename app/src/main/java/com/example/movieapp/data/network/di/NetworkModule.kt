5d84aceb2af525653481cce68cdda09b945dd4c9
Alex-mumo <alexymumo2000@gmail.com>
2022-01-07 07:50:13 +0300

    added di

--- app/src/main/java/com/example/movieapp/data/network/di/NetworkModule.kt
+++ app/src/main/java/com/example/movieapp/data/network/di/NetworkModule.kt
@@ -1,10 +1,37 @@
 package com.example.movieapp.data.network.di
 
+import com.example.movieapp.data.network.ApiService
+import com.example.movieapp.data.network.ApiService.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
-
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(provideOkhttp())
+            .build()
+            .create(ApiService::class.java)
 
     }
 }
+fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+    return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+}
+
+fun provideOkhttp(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(provideLoggingInterceptor())
+        .callTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+}
+
+
