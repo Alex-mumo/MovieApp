87fae876c3e54647b9d62d39154b9442f7762d21
Alex-mumo <alexymumo2000@gmail.com>
2022-01-09 15:34:57 +0300

    update

--- app/src/main/java/com/example/movieapp/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/activities/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.movieapp.presentation.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.movieapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navControler = navHostFragment.navController
+
+        navControler.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.movieFragment){
+
+            }
+        }
     }
 
 }
