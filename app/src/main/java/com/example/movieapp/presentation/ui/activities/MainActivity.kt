a0f6cb67866fefdc9c36379a0e63d6faa6d3117c
Alex-mumo <alexymumo2000@gmail.com>
2022-01-09 16:16:40 +0300

    update

--- app/src/main/java/com/example/movieapp/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import com.example.movieapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val bottomNav = findViewById<BottomNavigationMenuView>(R.id.bottom_nav)
         val navControler = navHostFragment.navController
+        //bottomNav?.setUpWithNavController(navControler)
 
         navControler.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.movieFragment){
--- app/src/main/java/com/example/movieapp/presentation/ui/adapters/MovieRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/adapters/MovieRecyclerViewAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.movieapp.presentation.ui.adapters
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.movieapp.domain.model.Movies
 
 class MovieRecyclerViewAdapter constructor(private val movies: List<Movies>) : RecyclerView.Adapter<MovieRecyclerViewHolder>() {
@@ -12,7 +15,12 @@ class MovieRecyclerViewAdapter constructor(private val movies: List<Movies>) : R
 
     //display the data at specified position
     override fun onBindViewHolder(holder: MovieRecyclerViewHolder, position: Int) {
-        TODO()
+        val movieItem = movies[position]
+        val context = holder.itemView.context
+        holder.bind(context, movieItem)
+        holder.itemView.setOnClickListener {
+            
+        }
     }
 
     override fun getItemCount(): Int {
@@ -21,5 +29,12 @@ class MovieRecyclerViewAdapter constructor(private val movies: List<Movies>) : R
 }
 
 class MovieRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(context: Context?, movieItem: Movies) {
+        Glide.with(context)
+            .load(movieItem.image)
+            .transition(DrawableTransitionOptions.withCrossFade(400))
+            .into()
+
+    }
 
 }
