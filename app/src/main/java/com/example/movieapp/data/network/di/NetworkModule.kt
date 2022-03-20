9c662c5f72f18a1ee1a84b881ce0ba6255c411a8
Alex-mumo <alexymumo2000@gmail.com>
2022-03-20 20:50:38 +0300

    updated network module

--- app/src/main/java/com/example/movieapp/data/network/di/NetworkModule.kt
+++ app/src/main/java/com/example/movieapp/data/network/di/NetworkModule.kt
@@ -14,6 +14,7 @@ val networkModule = module {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(providesOkHttp())
             .build()
             .create(ApiService::class.java)
     }
