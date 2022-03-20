f92067f6ada594c8b35e033f3eb1b59566360080
Alex-mumo <alexymumo2000@gmail.com>
2022-03-20 20:55:03 +0300

    added repos module

--- app/src/main/java/com/example/movieapp/data/repository/di/RepositoryModule.kt
+++ app/src/main/java/com/example/movieapp/data/repository/di/RepositoryModule.kt
@@ -1,1 +1,12 @@
 package com.example.movieapp.data.repository.di
+
+import com.example.movieapp.data.repository.datasources.MovieDetailRepositoryImpl
+import com.example.movieapp.data.repository.datasources.MovieRepositoryImpl
+import com.example.movieapp.domain.repositories.DetailRepository
+import com.example.movieapp.domain.repositories.MovieRepository
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+    single<DetailRepository> { MovieDetailRepositoryImpl(get(), get())}
+}
