7cee77aee6df972bcb702bccd860098960ce7316
Alex-mumo <alexymumo2000@gmail.com>
2022-03-20 20:32:58 +0300

    added koin

--- app/src/main/java/com/example/movieapp/MovieApplication.kt
+++ app/src/main/java/com/example/movieapp/MovieApplication.kt
@@ -2,6 +2,10 @@ package com.example.movieapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import timber.log.Timber
 
 @HiltAndroidApp
@@ -9,6 +13,14 @@ class MovieApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@MovieApplication)
+        }
     }
 
     private fun initTimber() {
